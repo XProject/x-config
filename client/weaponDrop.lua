85e3e3a640b72644dab7c2a62433d3c8f09dc2a9
Mahan Moulaei <mahanmoulaei@gmail.com>
2023-06-09 02:22:42 -0400

    refactor(client/weaponDrop): general code improvement

--- client/weaponDrop.lua
+++ client/weaponDrop.lua
@@ -1,4 +1,3 @@
-local playerId = PlayerId()
 local weaponHashes = {
     `PICKUP_WEAPON_ADVANCEDRIFLE`,
     `PICKUP_WEAPON_APPISTOL`,
@@ -86,31 +85,37 @@ local weaponHashes = {
     `PICKUP_WEAPON_WRENCH`
 }
 
----enable/disable picking up dropped weapon
+local api = setmetatable({}, {
+    __newindex = function(self, index, value)
+        exports(index, value)
+        rawset(self, index, value)
+    end
+})
+
+---Enables/Disables picking up dropped weapon
 ---@param state boolean
 ---@param weapon? string | number
-local function enablePickingUpDroppedWeapon(state, weapon)
-    if state == nil then return end
+function api.enablePickingUpDroppedWeapon(state, weapon)
+    if type(state) ~= "boolean" then return end
 
     if weapon then
         weapon = type(weapon) == "string" and joaat(weapon) or weapon
-        ToggleUsePickupsForPlayer(playerId, weapon, state)
+        ToggleUsePickupsForPlayer(cache.playerId, weapon, state)
         return
     end
 
     for i = 1, #weaponHashes do
-        ToggleUsePickupsForPlayer(playerId, weaponHashes[i], state)
+        ToggleUsePickupsForPlayer(cache.playerId, weaponHashes[i], state)
     end
 end
 
-CreateThread(function()
-    enablePickingUpDroppedWeapon(Config.EnablePickingUpDroppedWeapon)
-end)
+do api.enablePickingUpDroppedWeapon(Config.EnablePickingUpDroppedWeapon) end
 
-AddEventHandler("onResourceStop", function(resource)
-    if resource == GetCurrentResourceName() then
-        enablePickingUpDroppedWeapon(true)
-    end
-end)
+local function onResourceStop(resource)
+    if resource ~= cache.resource then return end
 
-exports("enablePickingUpDroppedWeapon", enablePickingUpDroppedWeapon)
+    enablePickingUpDroppedWeapon(true)
+end
+
+AddEventHandler("onResourceStop", onResourceStop)
+AddEventHandler("onClientResourceStop", onResourceStop)
