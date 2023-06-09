d154956f9b9c684c018a0a89fefb63f564c634a7
Mahan Moulaei <mahanmoulaei@gmail.com>
2023-06-09 02:15:27 -0400

    refactor(client/vehicleGenerator): use numeric for loop

--- client/vehicleGenerator.lua
+++ client/vehicleGenerator.lua
@@ -1,29 +1,31 @@
+local api = setmetatable({}, {
+    __newindex = function(self, index, value)
+        exports(index, value)
+        rawset(self, index, value)
+    end
+})
+
 ---enable/disable vehicles from generating in an area
 ---@param state boolean
 ---@param coords vector3
----@param range? number
-local function generateVehiclesInArea(state, coords, range)
-    if state == nil or not coords then return end
+---@param range? number defaults to 100.0 if not provided
+function api.generateVehiclesInArea(state, coords, range)
+    if type(state) ~= "boolean" or not coords then return end
 
     range = range or 100.0
 
     -- if state then
-    --     SetAllVehicleGeneratorsActiveInArea(coords.x - range, coords.y - range, coords.z - range, coords.x + range,
-    --         coords.y + range, coords.z + range, false, false)
+    --     SetAllVehicleGeneratorsActiveInArea(coords.x - range, coords.y - range, coords.z - range, coords.x + range, coords.y + range, coords.z + range, false, false)
     -- else
-    --     RemoveVehiclesFromGeneratorsInArea(coords.x - range, coords.y - range, coords.z - range, coords.x + range,
-    --         coords.y + range, coords.z + range)
+    --     RemoveVehiclesFromGeneratorsInArea(coords.x - range, coords.y - range, coords.z - range, coords.x + range, coords.y + range, coords.z + range)
     -- end
 
     -- experimental
-    SetAllVehicleGeneratorsActiveInArea(coords.x - range, coords.y - range, coords.z - range, coords.x + range,
-        coords.y + range, coords.z + range, state, state)
+    SetAllVehicleGeneratorsActiveInArea(coords.x - range, coords.y - range, coords.z - range, coords.x + range, coords.y + range, coords.z + range, state, state)
 end
 
-CreateThread(function()
-    for _, data in ipairs(Config.RemoveVehiclesInArea) do
-        generateVehiclesInArea(false, data.coords, data.range)
+do
+    for i = 1, #Config.RemoveVehiclesInArea do
+        api.generateVehiclesInArea(false, Config.RemoveVehiclesInArea[i].coords, Config.RemoveVehiclesInArea[i].range)
     end
-end)
-
-exports("generateVehiclesInArea", generateVehiclesInArea)
+end
