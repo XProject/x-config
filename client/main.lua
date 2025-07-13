427a3926c510bc8b40004a8de4304ee0b3ce418f
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2025-07-13 16:55:40 -0400

    fix(client): periodically apply instance priority setter
    especially for InstancePriorityHint which removes low-priority objects from the game environment

--- client/main.lua
+++ client/main.lua
@@ -1,3 +1,8 @@
+local function setInstancePriority()
+    SetInstancePriorityMode(1)
+    SetInstancePriorityHint(5) -- Each number between 0-5 has some effect on what objects are spawned mostly the low priority ones like garbage, which could be useful for someone if they are looking to get rid of these things quickly
+end
+
 CreateThread(function()
     if Config.DisableAmbience then
         StartAudioScene("CHARACTER_CHANGE_IN_SKY_SCENE")
@@ -25,8 +30,7 @@ CreateThread(function()
     SetFarDrawVehicles(false)
     SetDistantCarsEnabled(false)
 
-    SetInstancePriorityMode(1)
-    SetInstancePriorityHint(5)
+    setInstancePriority()
 
     SetMaxWantedLevel(Config.DisableWantedLevel and 0 or 5)
 
@@ -36,4 +40,6 @@ end)
 AddEventHandler("ox_lib:cache:ped", function(newPed)
     -- This flag is used to allow the ped to sprint in any interior
     SetPedConfigFlag(newPed, 427, true)
+
+    setInstancePriority() -- apparently the InstancePriority* functions are a setter, meaning they dont have to be looped, but based on my testings sometimes they need to be called again...
 end)
